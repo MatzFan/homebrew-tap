36c218d5c56bc09470174b43b966ae296b40c167
Bradley Curran <brad@bradleycurran.com>
2015-01-04 02:37:41 -0800

    quiver2jekyll 0.1.0

---
+++ quiver2jekyll.rb
@@ -0,0 +1,9 @@
+class Quiver2jekyll < Formula
+  homepage "https://github.com/bradley-curran/quiver2jekyll"
+  url "https://github.com/bradley-curran/quiver2jekyll/archive/0.1.0.tar.gz"
+  sha1 "84a15b05456d6669456842e866b0a35d2dd029d8"
+
+  def install
+    bin.install "quiver2jekyll"
+  end
+end
