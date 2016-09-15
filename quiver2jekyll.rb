8316e8f69aa771a7a58ab3fb2d8706a3ce3e157d
Bruce Steedman <MatzFan@users.noreply.github.com>
2016-09-15 19:49:30 +0100

    Update quiver2jekyll.rb sha256
    Fix sha1 deprecation warnings

--- quiver2jekyll.rb
+++ quiver2jekyll.rb
@@ -1,7 +1,7 @@
 class Quiver2jekyll < Formula
   homepage "https://github.com/bradley-curran/quiver2jekyll"
   url "https://github.com/bradley-curran/quiver2jekyll/archive/0.1.1.tar.gz"
-  sha1 "6dcd2389a8cb4c1b37ce1152c3f1c6810f8e9297"
+  sha256 "28041133079ce3da8577b48059a12b99a34a621c885b93204bd59b6a1ecd9e32"
 
   def install
     bin.install "quiver2jekyll"
